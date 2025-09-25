622e1e2472ef05d1776d9ade14dc8f21a797b66a
elctron-wrangler <wasnyder2@gmail.com>
2025-09-24 18:14:24 -0600

    Add files via upload

--- dmd_IO.vhd
+++ dmd_IO.vhd
@@ -2,12 +2,12 @@
 #- Company: LASP
 #- Engineer: William Snyder
 #-
-#- Create Date: 2025-09-12
+#- Create Date: 2025-09-24
 #- Design Name: SUMO APPS FPGA
 #- Project Name: SUMO
 #- Target Devices: XC7K160TFBG484-2
 #- Tool Versions: Vivado v2024.1
-#- Description: APPS FPGA top-level file
+#- Description: APPS FPGA Top-Level File
 #-
 #- Dependencies:
 #-
@@ -58,6 +58,7 @@ entity dmd_io is
     TX_EN                 : out std_logic;
     
     -- LVDS CONTROL
+    
     APPS_CNTL_DPN         : out std_logic;
     APPS_CNTL_DPP         : out std_logic;
     
@@ -198,6 +199,50 @@ end dmd_io;
 
 
 architecture behavioral of dmd_io is
+
+  -- Single-ended Driver for APPS_CNTL
+  signal APPS_CNTL_DP   : std_logic;
+  
+  -- Single-ended Drivers for BUS A
+  signal DDC_DIN_A0_DP  : std_logic;
+  signal DDC_DIN_A1_DP  : std_logic;
+  signal DDC_DIN_A2_DP  : std_logic;
+  signal DDC_DIN_A3_DP  : std_logic;
+  signal DDC_DIN_A4_DP  : std_logic;
+  signal DDC_DIN_A5_DP  : std_logic;
+  signal DDC_DIN_A6_DP  : std_logic;
+  signal DDC_DIN_A7_DP  : std_logic;
+  signal DDC_DIN_A8_DP  : std_logic;
+  signal DDC_DIN_A9_DP  : std_logic;
+  signal DDC_DIN_A10_DP : std_logic;
+  signal DDC_DIN_A11_DP : std_logic;
+  signal DDC_DIN_A12_DP : std_logic;
+  signal DDC_DIN_A13_DP : std_logic;
+  signal DDC_DIN_A14_DP : std_logic;
+  signal DDC_DIN_A15_DP : std_logic;
+  signal DVALID_A_DP    : std_logic;
+  signal DDC_DCLK_A_DP  : std_logic;
+
+  -- Single-ended Drivers for BUS B
+  signal DDC_DIN_B0_DP  : std_logic;
+  signal DDC_DIN_B1_DP  : std_logic;
+  signal DDC_DIN_B2_DP  : std_logic;
+  signal DDC_DIN_B3_DP  : std_logic;
+  signal DDC_DIN_B4_DP  : std_logic;
+  signal DDC_DIN_B5_DP  : std_logic;
+  signal DDC_DIN_B6_DP  : std_logic;
+  signal DDC_DIN_B7_DP  : std_logic;
+  signal DDC_DIN_B8_DP  : std_logic;
+  signal DDC_DIN_B9_DP  : std_logic;
+  signal DDC_DIN_B10_DP : std_logic;
+  signal DDC_DIN_B11_DP : std_logic;
+  signal DDC_DIN_B12_DP : std_logic;
+  signal DDC_DIN_B13_DP : std_logic;
+  signal DDC_DIN_B14_DP : std_logic;
+  signal DDC_DIN_B15_DP : std_logic;
+  signal DVALID_B_DP    : std_logic;
+  signal DDC_DCLK_B_DP  : std_logic;
+
 begin
     
     -- POWER SEQUENCE
@@ -216,93 +261,326 @@ begin
     
     -- RS422
     RS422_TX              <= '0';
-    RX_EN_n               <= '1'; -- Active low
-    TX_EN                 <= '0'; -- Active high
+    RX_EN_n               <= '1'; -- Active low to enable
+    TX_EN                 <= '0'; -- Active high to enable
     
-    -- LVDS CONTROL
-    APPS_CNTL_DPN         <= '0';
-    APPS_CNTL_DPP         <= '0';
+   
+    -- LVDS CONTROL - NOT USED
+
+    APP_CNTL_DP           <= '0';
+
+    OBUFDS_APPS_CNTL : OBUFDS
+    port
+    map (
+       O => APPS_CNTL_DPP,
+       OB => APPS_CNTL_DPN,
+       I => APPS_CNTL_DP);
+    
     
     -- LVDS BUS A
-    DDC_DIN_A0_DPN        <= '0';
-    DDC_DIN_A0_DPP        <= '0';
-    DDC_DIN_A1_DPN        <= '0';
-    DDC_DIN_A1_DPP        <= '0';
-    DDC_DIN_A2_DPN        <= '0';
-    DDC_DIN_A2_DPP        <= '0';
-    DDC_DIN_A3_DPN        <= '0';
-    DDC_DIN_A3_DPP        <= '0';
-    DDC_DIN_A4_DPN        <= '0';
-    DDC_DIN_A4_DPP        <= '0';
-    DDC_DIN_A5_DPN        <= '0';
-    DDC_DIN_A5_DPP        <= '0';
-    DDC_DIN_A6_DPN        <= '0';
-    DDC_DIN_A6_DPP        <= '0';
-    DDC_DIN_A7_DPN        <= '0';
-    DDC_DIN_A7_DPP        <= '0';
-    DDC_DIN_A8_DPN        <= '0';
-    DDC_DIN_A8_DPP        <= '0';
-    DDC_DIN_A9_DPN        <= '0';
-    DDC_DIN_A9_DPP        <= '0';
-    DDC_DIN_A10_DPN       <= '0';
-    DDC_DIN_A10_DPP       <= '0';
-    DDC_DIN_A11_DPN       <= '0';
-    DDC_DIN_A11_DPP       <= '0';
-    DDC_DIN_A12_DPN       <= '0';
-    DDC_DIN_A12_DPP       <= '0';
-    DDC_DIN_A13_DPN       <= '0';
-    DDC_DIN_A13_DPP       <= '0';
-    DDC_DIN_A14_DPN       <= '0';
-    DDC_DIN_A14_DPP       <= '0';
-    DDC_DIN_A15_DPN       <= '0';
-    DDC_DIN_A15_DPP       <= '0';
-    DVALID_A_DPN          <= '0';
-    DVALID_A_DPP          <= '0';
-    DDC_DCLK_A_DPN        <= '0';
-    DDC_DCLK_A_DPP        <= '0';
+    
+    DDC_DIN_A0_DP         <= '0';
+    DDC_DIN_A1_DP         <= '0';
+    DDC_DIN_A2_DP         <= '0';
+    DDC_DIN_A3_DP         <= '0';
+    DDC_DIN_A4_DP         <= '0';
+    DDC_DIN_A5_DP         <= '0';
+    DDC_DIN_A6_DP         <= '0';
+    DDC_DIN_A7_DP         <= '0';
+    DDC_DIN_A8_DP         <= '0';
+    DDC_DIN_A9_DP         <= '0';
+    DDC_DIN_A10_DP        <= '0';
+    DDC_DIN_A11_DP        <= '0';
+    DDC_DIN_A12_DP        <= '0';
+    DDC_DIN_A13_DP        <= '0';
+    DDC_DIN_A14_DP        <= '0';
+    DDC_DIN_A15_DP        <= '0';
+    DVALID_A_DP           <= '0';
+    DDC_DCLK_A_DP         <= '0';
+
+    OBUFDS_DDC_DIN_A0 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A0_DPP,
+        OB => DDC_DIN_A0_DPN,
+        I  => DDC_DIN_A0_DP);
+
+    OBUFDS_DDC_DIN_A1 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A1_DPP,
+        OB => DDC_DIN_A1_DPN,
+        I  => DDC_DIN_A1_DP);
+
+    OBUFDS_DDC_DIN_A2 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A2_DPP,
+        OB => DDC_DIN_A2_DPN,
+        I  => DDC_DIN_A2_DP);
+
+    OBUFDS_DDC_DIN_A3 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A3_DPP,
+        OB => DDC_DIN_A3_DPN,
+        I  => DDC_DIN_A3_DP);
+
+    OBUFDS_DDC_DIN_A4 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A4_DPP,
+        OB => DDC_DIN_A4_DPN,
+        I  => DDC_DIN_A4_DP);
+
+    OBUFDS_DDC_DIN_A5 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A5_DPP,
+        OB => DDC_DIN_A5_DPN,
+        I  => DDC_DIN_A5_DP);
+
+    OBUFDS_DDC_DIN_A6 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A6_DPP,
+        OB => DDC_DIN_A6_DPN,
+        I  => DDC_DIN_A6_DP);
+
+    OBUFDS_DDC_DIN_A7 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A7_DPP,
+        OB => DDC_DIN_A7_DPN,
+        I  => DDC_DIN_A7_DP);
+
+    OBUFDS_DDC_DIN_A8 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A8_DPP,
+        OB => DDC_DIN_A8_DPN,
+        I  => DDC_DIN_A8_DP);
+
+    OBUFDS_DDC_DIN_A9 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A9_DPP,
+        OB => DDC_DIN_A9_DPN,
+        I  => DDC_DIN_A9_DP);
+
+    OBUFDS_DDC_DIN_A10 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A10_DPP,
+        OB => DDC_DIN_A10_DPN,
+        I  => DDC_DIN_A10_DP);
+
+    OBUFDS_DDC_DIN_A11 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A11_DPP,
+        OB => DDC_DIN_A11_DPN,
+        I  => DDC_DIN_A11_DP);
+
+    OBUFDS_DDC_DIN_A12 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A12_DPP,
+        OB => DDC_DIN_A12_DPN,
+        I  => DDC_DIN_A12_DP);
+
+    OBUFDS_DDC_DIN_A13 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A13_DPP,
+        OB => DDC_DIN_A13_DPN,
+        I  => DDC_DIN_A13_DP);
+
+    OBUFDS_DDC_DIN_A14 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A14_DPP,
+        OB => DDC_DIN_A14_DPN,
+        I  => DDC_DIN_A14_DP);
+
+    OBUFDS_DDC_DIN_A15 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_A15_DPP,
+        OB => DDC_DIN_A15_DPN,
+        I  => DDC_DIN_A15_DP);
+
+    -- LVDS BUS A — control/clock
+    
+    OBUFDS_DVALID_A : OBUFDS
+    port
+    map (
+        O  => DVALID_A_DPP,
+        OB => DVALID_A_DPN,
+        I  => DVALID_A_DP);
+
+    OBUFDS_DDC_DCLK_A : OBUFDS
+    port
+    map (
+        O  => DDC_DCLK_A_DPP,
+        OB => DDC_DCLK_A_DPN,
+        I  => DDC_DCLK_A_DP);
+
     
     -- LVDS BUS B
-    DDC_DIN_B0_DPN        <= '0';
-    DDC_DIN_B0_DPP        <= '0';
-    DDC_DIN_B1_DPN        <= '0';
-    DDC_DIN_B1_DPP        <= '0';
-    DDC_DIN_B2_DPN        <= '0';
-    DDC_DIN_B2_DPP        <= '0';
-    DDC_DIN_B3_DPN        <= '0';
-    DDC_DIN_B3_DPP        <= '0';
-    DDC_DIN_B4_DPN        <= '0';
-    DDC_DIN_B4_DPP        <= '0';
-    DDC_DIN_B5_DPN        <= '0';
-    DDC_DIN_B5_DPP        <= '0';
-    DDC_DIN_B6_DPN        <= '0';
-    DDC_DIN_B6_DPP        <= '0';
-    DDC_DIN_B7_DPN        <= '0';
-    DDC_DIN_B7_DPP        <= '0';
-    DDC_DIN_B8_DPN        <= '0';
-    DDC_DIN_B8_DPP        <= '0';
-    DDC_DIN_B9_DPN        <= '0';
-    DDC_DIN_B9_DPP        <= '0';
-    DDC_DIN_B10_DPN       <= '0';
-    DDC_DIN_B10_DPP       <= '0';
-    DDC_DIN_B11_DPN       <= '0';
-    DDC_DIN_B11_DPP       <= '0';
-    DDC_DIN_B12_DPN       <= '0';
-    DDC_DIN_B12_DPP       <= '0';
-    DDC_DIN_B13_DPN       <= '0';
-    DDC_DIN_B13_DPP       <= '0';
-    DDC_DIN_B14_DPN       <= '0';
-    DDC_DIN_B14_DPP       <= '0';
-    DDC_DIN_B15_DPN       <= '0';
-    DDC_DIN_B15_DPP       <= '0';
-    DVALID_B_DPN          <= '0';
-    DVALID_B_DPP          <= '0';
-    DDC_DCLK_B_DPN        <= '0';
-    DDC_DCLK_B_DPP        <= '0';
+    
+    DDC_DIN_B0_DP         <= '0';
+    DDC_DIN_B1_DP         <= '0';
+    DDC_DIN_B2_DP         <= '0';
+    DDC_DIN_B3_DP         <= '0';
+    DDC_DIN_B4_DP         <= '0';
+    DDC_DIN_B5_DP         <= '0';
+    DDC_DIN_B6_DP         <= '0';
+    DDC_DIN_B7_DP         <= '0';
+    DDC_DIN_B8_DP         <= '0';
+    DDC_DIN_B9_DP         <= '0';
+    DDC_DIN_B10_DP        <= '0';
+    DDC_DIN_B11_DP        <= '0';
+    DDC_DIN_B12_DP        <= '0';
+    DDC_DIN_B13_DP        <= '0';
+    DDC_DIN_B14_DP        <= '0';
+    DDC_DIN_B15_DP        <= '0';
+    DVALID_B_DP           <= '0';
+    DDC_DCLK_B_DP         <= '0';
+    
+    OBUFDS_DDC_DIN_B0 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B0_DPP,
+        OB => DDC_DIN_B0_DPN,
+        I  => DDC_DIN_B0_DP);
+
+    OBUFDS_DDC_DIN_B1 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B1_DPP,
+        OB => DDC_DIN_B1_DPN,
+        I  => DDC_DIN_B1_DP);
+
+    OBUFDS_DDC_DIN_B2 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B2_DPP,
+        OB => DDC_DIN_B2_DPN,
+        I  => DDC_DIN_B2_DP);
+
+    OBUFDS_DDC_DIN_B3 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B3_DPP,
+        OB => DDC_DIN_B3_DPN,
+        I  => DDC_DIN_B3_DP);
+
+    OBUFDS_DDC_DIN_B4 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B4_DPP,
+        OB => DDC_DIN_B4_DPN,
+        I  => DDC_DIN_B4_DP);
+
+    OBUFDS_DDC_DIN_B5 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B5_DPP,
+        OB => DDC_DIN_B5_DPN,
+        I  => DDC_DIN_B5_DP);
+
+    OBUFDS_DDC_DIN_B6 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B6_DPP,
+        OB => DDC_DIN_B6_DPN,
+        I  => DDC_DIN_B6_DP);
+
+    OBUFDS_DDC_DIN_B7 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B7_DPP,
+        OB => DDC_DIN_B7_DPN,
+        I  => DDC_DIN_B7_DP);
+
+    OBUFDS_DDC_DIN_B8 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B8_DPP,
+        OB => DDC_DIN_B8_DPN,
+        I  => DDC_DIN_B8_DP);
+
+    OBUFDS_DDC_DIN_B9 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B9_DPP,
+        OB => DDC_DIN_B9_DPN,
+        I  => DDC_DIN_B9_DP);
+
+    OBUFDS_DDC_DIN_B10 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B10_DPP,
+        OB => DDC_DIN_B10_DPN,
+        I  => DDC_DIN_B10_DP);
+
+    OBUFDS_DDC_DIN_B11 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B11_DPP,
+        OB => DDC_DIN_B11_DPN,
+        I  => DDC_DIN_B11_DP);
+
+    OBUFDS_DDC_DIN_B12 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B12_DPP,
+        OB => DDC_DIN_B12_DPN,
+        I  => DDC_DIN_B12_DP);
+
+    OBUFDS_DDC_DIN_B13 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B13_DPP,
+        OB => DDC_DIN_B13_DPN,
+        I  => DDC_DIN_B13_DP);
+
+    OBUFDS_DDC_DIN_B14 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B14_DPP,
+        OB => DDC_DIN_B14_DPN,
+        I  => DDC_DIN_B14_DP);
+
+    OBUFDS_DDC_DIN_B15 : OBUFDS
+    port
+    map (
+        O  => DDC_DIN_B15_DPP,
+        OB => DDC_DIN_B15_DPN,
+        I  => DDC_DIN_B15_DP);
+
+    -- LVDS BUS B — control/clock
+    
+    OBUFDS_DVALID_B : OBUFDS
+    port
+    map (
+        O  => DVALID_B_DPP,
+        OB => DVALID_B_DPN,
+        I  => DVALID_B_DP);
+
+    OBUFDS_DDC_DCLK_B : OBUFDS
+    port
+    map (
+        O  => DDC_DCLK_B_DPP,
+        OB => DDC_DCLK_B_DPN,
+        I  => DDC_DCLK_B_DP);
+
     
     -- CONTROL OUTPUTS
-    COMP_DATA             <= '0';
-    NS_FLIP               <= '0';
-    WDT_ENBLZ             <= '1'; -- Active low
+    COMP_DATA             <= '0'; -- No dynamic change
+    NS_FLIP               <= '0'; -- No dynamic change
+    WDT_ENBLZ             <= '1'; -- Active low, 10 second auto reset
     ROWMD_0               <= '0';
     ROWMD_1               <= '0';
     ROWAD_0               <= '0';
@@ -316,18 +594,18 @@ begin
     ROWAD_8               <= '0';
     ROWAD_9               <= '0';
     ROWAD_10              <= '0';
-    RST2BLKZ              <= '0';
+    RST2BLKZ              <= '0'; -- No dynamic change
     BLKMD_0               <= '0';
     BLKMD_1               <= '0';
     BLKAD_0               <= '0';
     BLKAD_1               <= '0';
     BLKAD_2               <= '0';
     BLKAD_3               <= '0';
-    ARSTZ                 <= '0'; -- Active low
-    CLKIN_R               <= '0';
+    ARSTZ                 <= '1'; -- Active low
+    CLKIN_R               <= '0'; -- Reference clock
     STEPVCC               <= '0'; -- Not used
-    USR_TEST              <= '0';
-    PWR_FLOAT             <= '0';
+    USR_TEST              <= '0'; -- LED, D4
+    PWR_FLOAT             <= '0'; -- High floats mirrors, power cycle to reset
     DDC_SPARE_0           <= '1'; -- Renamed LOAD4, Active low
     DDC_SPARE_1           <= '0'; -- Not used
 
@@ -344,29 +622,4 @@ begin
     FPGA_D9               <= '0';
 
 
-    -- OBUFDS: Differential Output Buffer
-    --         7 Series
-    -- Xilinx HDL Language Template, version 2025.1
-
-    OBUFDS_inst : OBUFDS
-    generic map (
-       IOSTANDARD => "DEFAULT", -- Specify the output I/O standard
-       SLEW => "SLOW")          -- Specify the output slew rate
-    port map (
-       O => O,     -- Diff_p output (connect directly to top-level port)
-       OB => OB,   -- Diff_n output (connect directly to top-level port)
-       I => I      -- Buffer input
-    );
-
-    -- End of OBUFDS_inst instantiation
-
-
 end behavioral;
-
-The LVDS outputs will need to have a differential I0 buffer instantiated in the code, example:
-OBUFDS 05 : OBUFDS
-port
-map(
-0 => megs_a_cmd_clk_p,
-OB=> megs_a_cmd_clk_n,
-I => megs_a_cmd_clk);
